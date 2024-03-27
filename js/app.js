baaf1482331ea52aefddb28f25c28c903c3d469a
Felipe Marano <felipehenriquemarano@gmail.com>
2024-03-27 15:07:19 -0300

    função tipo e qtd

--- js/app.js
+++ js/app.js
@@ -2,5 +2,6 @@ function comprar () {
     let tipo = document.getElementById ("tipo-ingresso");
     let qtd = document.getElementById ("qtd");
 
-
+    // alert (tipo.value); = para testar se o botao para o "tipo" esta funcionando
+    // alert (qtd.value); = para testar se o botao para a "qtd" esta funcionando
 }
